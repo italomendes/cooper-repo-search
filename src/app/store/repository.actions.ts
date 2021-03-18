f3d875ccf3424caa2fc1ed4a1ecbf7d3cec058e5
italomendes <italomemendes@gmail.com>
2021-03-18 10:29:36 -0300

    Criado a action de busca de repositórios.

--- src/app/store/repository.actions.ts
+++ src/app/store/repository.actions.ts
@@ -1,3 +1,12 @@
+import { Action } from "@ngrx/store";
+
 export enum RepositoryActionTypes {
     REPOSITORIES_BY_USER = "[REPOSITORIES_BY_USER] Get all repositories by user"
 }
+
+export class RepositoriesByUser implements Action {
+    readonly type = RepositoryActionTypes.REPOSITORIES_BY_USER;
+    constructor(public payload: {userName: string}) {}
+}
+
+export type RepositoriesActions = RepositoriesByUser;
